30dca0cc182d125c0d8f525f4b7d2855cf672204
jamgi <p-bin@naver.com>
2026-03-29 15:18:22 +0900

    KB : [Feat] 서버에 임시 DB 파일 연결
    - url에 기존/supplements하면 제가 만든 app.js가 열림.

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('supplements') 
+  async getSupplements() {
+    const data = await this.prisma.supplements.findMany({ take: 3 });
+    
+    // BigInt 처리 (JSON 변환)
+    return JSON.parse(
+      JSON.stringify(data, (key, value) =>
+        typeof value === 'bigint' ? value.toString() : value,
+      ),
+    );
   }
 }
